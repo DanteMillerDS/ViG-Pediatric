825568f961ff614850594b640a10d866185a46cf
Dante Miller <dantemillerpersonal@gmail.com>
2024-03-30 22:10:32 -0500

    Change paths

--- scripts/pvig/finetune_pvig_b_224_gelu.py
+++ scripts/pvig/finetune_pvig_b_224_gelu.py
@@ -1,6 +1,6 @@
 import sys
 import os
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 import data_loader.extract_data as extract_data
