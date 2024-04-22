90c0f9ec31bc91ccd8bd0454ccb938fbf28da868
Dante Miller <dantemillerpersonal@gmail.com>
2024-04-21 19:10:51 -0500

    Switch pvt_v2_b2 to pvt_v2_b1

--- fine_tune/finetune.py
+++ fine_tune/finetune.py
@@ -12,7 +12,7 @@ from model.vig_model import vig_ti_224_gelu, vig_s_224_gelu, vig_b_224_gelu
 from model.pvig_model import pvig_ti_224_gelu, pvig_s_224_gelu, pvig_b_224_gelu, pvig_m_224_gelu
 import torchvision.models as models
 from model.custom_model import Model
-from PVT.classification.pvt_v2 import pvt_v2_b2_li
+from PVT.classification.pvt_v2 import pvt_v2_b1
 
 class TrainModelClassifier:
     def __init__(self, medical_type, model_name, mean_and_std, epochs=50):
@@ -28,7 +28,7 @@ class TrainModelClassifier:
             "pvig_s_224_gelu": pvig_s_224_gelu,
             "pvig_m_224_gelu": pvig_m_224_gelu,
             "pvig_b_224_gelu": pvig_b_224_gelu,
-            "pvt_v2_b2": pvt_v2_b2_li,
+            "pvt_v2_b1": pvt_v2_b1,
             "resnet50": models.resnet50(),
             "efficientnetb5": models.efficientnet_b5(),
             "densenet161": models.densenet161(),
